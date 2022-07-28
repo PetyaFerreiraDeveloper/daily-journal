02f2f8ee466e7db3b60f541fc6cc39aa5b3be327
Petya <petianaidenova@gmail.com>
2022-07-28 12:35:59 +0200

    close burger menu onclickoutside

--- src/components/header/SideNavigation.js
+++ src/components/header/SideNavigation.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import BurgerMenu from './BurgerMenu';
+import {ClickOutside} from '../../utils/ClickOutside';
 
 const SideNavigation = ({ openMenu, setOpenMenu }) => {
-  // let menuRef = useRef();
-
-  // useEffect(() => {
-    
-  //     document.addEventListener('click', (e) => {
-  //      if (!menuRef.current.contains(e.target)) {
-  //       setOpenMenu(false);
-  //      }
-  //     })
-  // })
+  const node = useRef();
+  ClickOutside(node, () => setOpenMenu(false));
 
   const showMenu =
     'fixed right-0 top-0 h-screen overflow-y-auto bg-white transition ease-in-out delay-150 rounded-tl-3xl rounded-bl-3xl -mx-1 text-gray-800 pb-10';
 
   return (
-    <nav  className="2xl:hidden" onClick={() => setOpenMenu(!openMenu)}  >
+    <nav  
+      className="2xl:hidden" 
+      onClick={() => setOpenMenu(!openMenu)}  
+      ref={node}
+    >
       {!openMenu ? (
           <BurgerMenu  className={`${showMenu} translate-x-full`} />
       ) : (
---
+++ src/utils/ClickOutside.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+
+export const ClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event?.target)) {
+        return;
+      }
+
+      handler();
+    };
+
+    document.addEventListener('mousedown', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+    };
+  }, [ref, handler]);
+};
