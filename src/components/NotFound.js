ea011c02949156a7aa75de42885a6d7014871bb3
Petya <petianaidenova@gmail.com>
2022-08-03 10:49:08 +0200

    update not found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+  const backClickHandler = () => {
+    navigate(-1);
+};
   return (
-    <div>404 Page not found</div>
-  )
-}
+    <div className="flex flex-col gap-y-10 justify-center items-center">
+      <div>404 Page not found</div>
+      <button 
+            className="rounded-full border-2 border-orange-500 bg-orange-400 px-8 py-2"
+            onClick={backClickHandler}
+        >
+            Back
+        </button>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
