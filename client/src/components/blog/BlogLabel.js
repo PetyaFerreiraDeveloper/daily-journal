2ec761d9f75a867fbdef2426f380e4bdda2f86b1
Petya <petianaidenova@gmail.com>
2022-08-06 11:17:41 +0200

    add blog label

---
+++ client/src/components/blog/BlogLabel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BlogLabel = () => {
+  return (
+    <p className="bg-gray-800 border-2  border-gray-900 px-2 py-1 rounded-xl absolute top-0 right-0 uppercase">blog</p>
+  )
+}
+
+export default BlogLabel
--- client/src/components/myJournal/JournalEntry.js
+++ client/src/components/myJournal/JournalEntry.js
@@ -3,6 +3,7 @@ import React from "react";
 import { dateFormatter } from "../../utils/dateFormatter";
 import { capitalizeFirstLetter } from "../../utils/capitalizeFirstLetter";
 import Button from "../common/Button";
+import BlogLabel from "../blog/BlogLabel";
 
 const JournalEntry = (props) => {
   const { journalEntry } = props;
@@ -15,21 +16,22 @@ const JournalEntry = (props) => {
 
   return (
     <article
-      className={`flex flex-col p-4 w-[350px] border-2 border-gray-600 rounded-2xl shadow-[0_2px_4px_1px_rgba(190,193,202,0.5)] drop-shadow-xl text-white ${blogBackground}`}
+      className={`static flex flex-col justify-center gap-y-4 p-4 w-[350px] h-[200px] border-2 border-gray-600 rounded-2xl shadow-[0_2px_4px_1px_rgba(190,193,202,0.5)] drop-shadow-xl text-white ${blogBackground}`}
     >
-        <p className="self-end">{dateFormatter(journalEntry._createdOn)}</p>
-      <div>
-        <p className="italic">{capitalizeFirstLetter(journalEntry.category)}</p>
-        <h3 className="text-ellipsis overflow-hidden"> {capitalizeFirstLetter(journalEntry.title)}</h3>
-        {journalEntry.blog ? (
-          <p>{`author: ${capitalizeFirstLetter(author)}`}</p>
-        ) : null}
-      </div>
+      {journalEntry.blog ? <BlogLabel />: null}
+      
+        <div className="flex justify-between">
+          <p>{dateFormatter(journalEntry._createdOn)}</p>
+          {journalEntry.blog ? <p>{`author: ${capitalizeFirstLetter(author)}`}</p> : null}
+        </div>
+        <h3 className="text-center text-ellipsis overflow-hidden">
+          {capitalizeFirstLetter(journalEntry.title)}
+        </h3>
 
       <Button
         nav={`/entry/${journalEntry._id}`}
         label={"Details"}
-        className={"bg-orange-400 border-orange-500 self-end"}
+        className={"bg-orange-400 border-orange-500 self-end -mb-5"}
       />
     </article>
   );
