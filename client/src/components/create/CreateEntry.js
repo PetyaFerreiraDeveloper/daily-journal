2b2d4c0a4d6d2a3b1b0184c17ff25840e1232d84
Petya <petianaidenova@gmail.com>
2022-08-09 20:00:15 +0200

    validate input on create

--- client/src/components/create/CreateEntry.js
+++ client/src/components/create/CreateEntry.js
@@ -7,7 +7,8 @@ import * as journalService from "../../services/journalService";
 
 const CreateEntry = () => {
   const { addEntry } = useContext(JournalContext);
-  // const [errors, setErrors] = useState({});
+  const [formError, setFormError] = useState("");
+  const [errors, setErrors] = useState({});
 
   const [newEntry, setNewEntry] = useState({
     title: "",
@@ -21,6 +22,20 @@ const CreateEntry = () => {
 
   const createHandler = (e) => {
     e.preventDefault();
+
+    if(newEntry.title === '' || newEntry.journalEntry === '') {
+      setFormError(true);
+      return;
+    };
+    
+    if (newEntry.title !== '' && newEntry.journalEntry !== '') {
+      setFormError(false);
+    };
+    
+    if(isFormNotValid) {
+      return;
+    }
+
     journalService.create(newEntry).then((result) => {
       addEntry(result);
     });
@@ -38,94 +53,118 @@ const CreateEntry = () => {
       [e.target.name]:
         e.target.type === "checkbox" ? e.target.checked : e.target.value,
     }));
+    if(!errors) {
+      setFormError(false);
+    }
   };
 
-  // const isFormNotValid = !Object.values(newEntry).some((x) => x === "");
+  const minLength = (e, limit) => {
+    setErrors((state) => ({
+      ...state,
+      [e.target.name]: newEntry[e.target.name].length < limit,
+    }));
+  };
+
+  const isFormNotValid = Object.values(errors).some((x) => x);
+
   return (
     <div className="">
       <section
         className={`-mt-[80px] md:-mt-[116px] h-20 md:h-[130px] -mx-[32px] lg:-mx-[96px] 2xl:-mx-[128px] px-8 md:px-16 2xl:px-32 justify-center items-center xs:pb-10 pb-20 relative bg-heroGreen`}
-      >
-      </section>
+      ></section>
       <div className="md:-mt-32">
-      <form
-        onSubmit={createHandler}
-        id="register"
-        className="flex flex-col md:h-[900px] justify-center border-2 m-auto gap-y-6 py-20 px-6 bg-white"
-      >
-        <div className="flex flex-col gap-y-2 ">
-          <div className="flex gap-x-3 border-b-2">
-            <label htmlFor="title">Title:</label>
-            <input
-              type="text"
-              id="title"
-              name="title"
-              placeholder="Entry Title..."
-              className="outline-hidden focus:outline-none w-full"
-              onChange={onChange}
-              value={newEntry.title}
-            />
-          </div>
-
-          <div className="flex gap-x-3 border-b-2">
-            <label htmlFor="category">Category:</label>
-            <input
-              type="text"
-              id="category"
-              name="category"
-              placeholder="Fun"
-              className="outline-hidden focus:outline-none w-full"
-              onChange={onChange}
-              value={newEntry.category}
-            />
-          </div>
-          <div className="flex gap-x-3 border-b-2">
-            <label htmlFor="blog">Share this record on the Blog:</label>
-            <input
-              type="checkbox"
-              name="blog"
-              id="blog"
-              className="w-5"
-              checked={newEntry.blog}
-              onChange={onChange}
-            />
-          </div>
-          {newEntry.blog ? (
+        <form
+          onSubmit={createHandler}
+          className="flex flex-col md:h-[900px] justify-center border-2 m-auto gap-y-6 py-20 px-6 bg-white"
+        >
+          <div className="flex flex-col gap-y-2 ">
             <div className="flex gap-x-3 border-b-2">
-              <label htmlFor="authorName">Author Name: </label>
+              <label htmlFor="title">Title:</label>
               <input
                 type="text"
-                id="authorName"
-                name="authorName"
-                placeholder="Name..."
-                className="max-w-[135px] focus:outline-none"
+                id="title"
+                name="title"
+                placeholder="Entry Title..."
+                className="outline-hidden focus:outline-none w-full"
                 onChange={onChange}
-                value={newEntry.authorName}
+                value={newEntry.title}
+                onBlur={(e) => minLength(e, 3)}
               />
             </div>
-          ) : null}
-          <div className="w-full">
-            <textarea
-              name="journalEntry"
-              id="journalEntry"
-              placeholder="Start your entry today"
-              rows="9"
-              className="border-2 w-full focus:outline-none"
-              onChange={onChange}
-              value={newEntry.journalEntry}
-            ></textarea>
-          </div>
+            {errors.title && (
+              <p className="text-red-500">
+                Title should be at least three characters long
+              </p>
+            )}
+            <div className="flex gap-x-3 border-b-2">
+              <label htmlFor="category">Category:</label>
+              <input
+                type="text"
+                id="category"
+                name="category"
+                placeholder="Fun"
+                className="outline-hidden focus:outline-none w-full"
+                onChange={onChange}
+                value={newEntry.category}
+              />
+            </div>
+            <div className="flex gap-x-3 border-b-2">
+              <label htmlFor="blog">Share this record on the Blog:</label>
+              <input
+                type="checkbox"
+                name="blog"
+                id="blog"
+                className="w-5"
+                checked={newEntry.blog}
+                onChange={onChange}
+              />
+            </div>
+            {newEntry.blog ? (
+              <div className="flex gap-x-3 border-b-2">
+                <label htmlFor="authorName">Author Name: </label>
+                <input
+                  type="text"
+                  id="authorName"
+                  name="authorName"
+                  placeholder="Name..."
+                  className="max-w-[135px] focus:outline-none"
+                  onChange={onChange}
+                  value={newEntry.authorName}
+                />
+              </div>
+            ) : null}
 
-          <input
-            className={`border-2 bg-dark-green border-darker-green text-white rounded-full  px-8 py-2 place-self-center cursor-pointer`}
-            type="submit"
-            value="Create"
-            // disabled={!isFormNotValid}
-          />
-        </div>
-      </form>
+            <div className="w-full">
+              <textarea
+                name="journalEntry"
+                id="journalEntry"
+                placeholder="Start your entry today"
+                rows="9"
+                className="border-2 w-full focus:outline-none"
+                onChange={onChange}
+                value={newEntry.journalEntry}
+                onBlur={(e) => minLength(e, 3)}
+              ></textarea>
+            </div>
+            {errors.journalEntry && (
+              <p className="text-red-500">
+                Your journal entry should be at least three characters long
+              </p>
+            )}
+            {(formError || isFormNotValid ) ? (
+              <p className="text-red-500">
+                Please fill out both entry title and entry text to create a
+                record
+              </p>
+            ) : null }
+            <input
+              className={`border-2 bg-dark-green border-darker-green text-white rounded-full  px-8 py-2 place-self-center cursor-pointer`}
+              type="submit"
+              value="Create"
+            />
+          </div>
+        </form>
       </div>
-   
     </div>
   );
 };
